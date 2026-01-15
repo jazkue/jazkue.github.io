d7569a2ba7d466706ff5451f8e7ff70b4537bf8c
ccassetto <javierdeazkue@gmail.com>
2026-01-15 19:14:36 -0300

    more tests

--- js/sketch.js
+++ js/sketch.js
@@ -1,30 +1,29 @@
 let canvas;
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   canvas.position(0, 0);
   canvas.style("z-index", "-1");
   canvas.style("pointer-events", "none"); // important
+  noSmooth();
 }
 
 function draw() {
   background(255);
   noFill();
   strokeWeight(1);
-
-  translate(width / 2, height / 2);
-  scale(1.5);
-  translate(-width / 2, -height / 2);
+  scale(1.2);
+  translate(-width/2, -height/2);
 
   let time = millis() / 200.0;
 
-  for (let i = 0; i < height; i += 10) {
+  for (let i = 0; i < height; i += 20) {
     push();
     translate(sin((time + i) / 50) * 20, 0);
     beginShape();
-    for (let x = 0; x < width; x += 5) {
+    for (let x = 0; x < width; x += 100) {
       let freq = map(x, 0, width, 6, 24);
-      let y = sin(x / freq + time) * 15;
+      let y = sin(x / freq + time) * map(mouseX,0,width,15,75);
       vertex(x, y + i);
     }
     endShape();
