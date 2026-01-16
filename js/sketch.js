09d14efbfac28506150cd32a0ee8c72d96a77665
ccassetto <javierdeazkue@gmail.com>
2026-01-16 14:26:45 -0300

    mouseY test

--- js/sketch.js
+++ js/sketch.js
@@ -15,7 +15,7 @@ function draw() {
   scale(1.2);
   translate(-width/2, -height/2);
 
-  let time = millis() / map(mouseY,height, 0, 50.0, 200.0);
+  let time = (millis() / 200.0) * map(mouseY,0,height,0.5,1.5);
 
   for (let i = 0; i < height; i += 20) {
     push();
